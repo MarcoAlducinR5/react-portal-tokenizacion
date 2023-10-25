9ac5a881c00ce7544aa326384ede285bee5e03ec
Marco Antonio Alducin Garcia <sokram_r5_1992@hotmail.com>
2023-10-24 22:54:45 -0600

    Componente del titulo y texto identificador

--- src/Main/index.js
+++ src/Main/index.js
@@ -3,8 +3,17 @@ import React from 'react';
 function Main() {
     return (
       <>
-        <div class="flex flex-col justify-center items-center px-8 py-[88px]">
-            <div class="w-[320px] h-[1900px] lg:w-[728px] lg:h-[1282px] border-[1px] border-solid border-[#E0E0E0] rounded-[10px] gap-5 bg-[#ffffff]">
+        <div class="flex flex-col justify-center items-center ">
+          <div className='lg:border-[1px] border-sky-gray-500 rounded-[10px] mx-9 my-[125px] lg:mx-0 lg:my-[153px] w-[320px] h-[1900px] lg:w-[728px] lg:h-[1282px] lg:px-[18px] lg:py-8'>
+            <div className='flex justify-between items-start flex-col lg:flex-row text-[#292929] border-b-[1px] border-sky-gray-500 pb-6'>
+              <div>
+                <span className='text-[22px] font-medium'>Pago con tarjeta</span>
+                <br/>
+                <span className='text-lg font-normal'>Ingresa los datos de tu tarjeta</span>
+              </div>
+              <br className='lg:hidden' />
+              <span className='text-base font-normal'>Identificador: 12345678</span>
+            </div>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda a quidem enim, quibusdam laborum impedit in soluta similique, repellat dolore neque quas distinctio? Molestiae ab impedit officia odio placeat earum.
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda a quidem enim, quibusdam laborum impedit in soluta similique, repellat dolore neque quas distinctio? Molestiae ab impedit officia odio placeat earum.
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda a quidem enim, quibusdam laborum impedit in soluta similique, repellat dolore neque quas distinctio? Molestiae ab impedit officia odio placeat earum.
@@ -16,7 +25,7 @@ function Main() {
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda a quidem enim, quibusdam laborum impedit in soluta similique, repellat dolore neque quas distinctio? Molestiae ab impedit officia odio placeat earum.
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda a quidem enim, quibusdam laborum impedit in soluta similique, repellat dolore neque quas distinctio? Molestiae ab impedit officia odio placeat earum.
             
-            </div>
+          </div>
         </div> 
       </>
     );
