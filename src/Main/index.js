4fe38a6c957f7591448eb141daaa754833b17581
Marco Antonio Alducin Garcia <sokram_r5_1992@hotmail.com>
2023-10-26 18:31:22 -0600

    Seccion Datos de tarjetahabiente

--- src/Main/index.js
+++ src/Main/index.js
@@ -40,8 +40,115 @@ function Main() {
                 </div>
               </label>
             </div>
+            
+            <span className='text-xl text-sky-black-100 text font-normal'>Datos de tarjetahabiente</span>
 
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda a quidem enim, quibusdam laborum impedit in soluta similique, repellat dolore neque quas distinctio? Molestiae ab impedit officia odio placeat earum.
+            <div className='flex flex-col lg:flex-row justify-between py-2 lg:py-4'>
+              <div className='flex flex-col h-[86px] lg:w-[205px] lg:h-[80px]'>
+                <div className='block my-1 lg:mt-0 lg:mb-1'>
+                  <label className='text-sm text-[#292929]'>Nombre</label>
+                </div>
+                <div className='block my-1 lg:mb-0 lg:mt-1'>
+                  <input type='text' name='nombre' placeholder='Nombre' className='w-full h-[40px] lg:h-[48px] rounded border-[1px] border-sky-gray-500 focus:outline-none focus:border-[#DA1E28] placeholder:text-base placeholder:text-sky-gray-120 placeholder:p-4' />
+                </div>
+              </div>
+              <div className='flex flex-col h-[86px] lg:w-[205px] lg:h-[80px]'>
+                <div className='block my-1 lg:mt-0 lg:mb-1'>
+                  <label className='text-sm text-[#292929]'>Apellido paterno</label>
+                </div>
+                <div className='block my-1 lg:mb-0 lg:mt-1'>
+                  <input type='text' name='apellidoPaterno' placeholder='Apellido paterno' className='w-full h-[40px] lg:h-[48px] rounded border-[1px] border-sky-gray-500 focus:outline-none focus:border-[#DA1E28] placeholder:text-base placeholder:text-sky-gray-120 placeholder:p-4' />
+                </div>
+              </div>
+              <div className='flex flex-col h-[86px] lg:w-[205px] lg:h-[80px]'>
+                <div className='block my-1 lg:mt-0 lg:mb-1'>
+                  <label className='text-sm text-[#292929]'>Apellido materno</label>
+                </div>
+                <div className='block my-1 lg:mb-0 lg:mt-1'>
+                  <input type='text' name='apellidoMaterno' placeholder='Apellido materno' className='w-full h-[40px] lg:h-[48px] rounded border-[1px] border-sky-gray-500 focus:outline-none focus:border-[#DA1E28] placeholder:text-base placeholder:text-sky-gray-120 placeholder:p-4' />
+                </div>
+              </div>
+            </div>
+
+            <div className='flex flex-col lg:flex-row justify-between py-2 lg:py-4'>
+              <div className='flex flex-col h-[86px] lg:w-[446.5px] lg:h-[80px]'>
+                <div className='block my-1 lg:mt-0 lg:mb-1'>
+                  <label className='text-sm text-[#292929]'>Correo electrónico</label>
+                </div>
+                <div className='block my-1 lg:mb-0 lg:mt-1'>
+                  <input type='email' name='correo' placeholder='mail@mail.com' className='w-full h-[40px] lg:h-[48px] rounded border-[1px] border-sky-gray-500 focus:outline-none focus:border-[#DA1E28] placeholder:text-base placeholder:text-sky-gray-120 placeholder:p-4' />
+                </div>
+              </div>
+              <div className='flex flex-col h-[86px] lg:w-[205px] lg:h-[80px]'>
+                <div className='block my-1 lg:mt-0 lg:mb-1'>
+                  <label className='text-sm text-[#292929]'>Número telefónico</label>
+                </div>
+                <div className='block my-1 lg:mb-0 lg:mt-1'>
+                  <input type='tel' name='telefono' placeholder='Teléfono' className='w-full h-[40px] lg:h-[48px] rounded border-[1px] border-sky-gray-500 focus:outline-none focus:border-[#DA1E28] placeholder:text-base placeholder:text-sky-gray-120 placeholder:p-4' />
+                </div>
+              </div>
+            </div>
+
+            <div className='flex flex-col lg:flex-row justify-between py-2 lg:py-4'>
+              <div className='flex flex-col h-[86px] lg:w-[326px] lg:h-[80px]'>
+                <div className='block my-1 lg:mt-0 lg:mb-1'>
+                  <label className='text-sm text-[#292929]'>Número de tarjeta</label>
+                </div>
+                <div className='block my-1 lg:mb-0 lg:mt-1'>
+                  <input type='tel' name='tarjeta' placeholder='**** **** **** 0123' className='w-full h-[40px] lg:h-[48px] rounded border-[1px] border-sky-gray-500 focus:outline-none focus:border-[#DA1E28] placeholder:text-base placeholder:text-sky-gray-120 placeholder:p-4' />
+                </div>
+              </div>
+              <div className='flex flex-col h-[86px] lg:w-[326px] lg:h-[80px]'>
+                <div className='block my-1 lg:mt-0 lg:mb-1'>
+                  <label className='text-sm text-[#292929]'>Confirmar número de tarjeta</label>
+                </div>
+                <div className='block my-1 lg:mb-0 lg:mt-1'>
+                  <input type='tel' name='confirmaTarjeta' placeholder='**** **** **** 0123' className='w-full h-[40px] lg:h-[48px] rounded border-[1px] border-sky-gray-500 focus:outline-none focus:border-[#DA1E28] placeholder:text-base placeholder:text-sky-gray-120 placeholder:p-4' />
+                </div>
+              </div>
+            </div>
+
+            <div className='flex flex-col lg:flex-row justify-between py-2 lg:py-4'>
+              <div className='flex flex-col h-[86px] lg:w-[205px] lg:h-[80px]'>
+                <div className='block my-1 lg:mt-0 lg:mb-1'>
+                  <label className='text-sm text-[#292929]'>Mes de vencimiento</label>
+                </div>
+                <div className='block my-1 lg:mb-0 lg:mt-1'>
+                  <select name='mesVenci' placeholder='Mes' class='w-full h-[40px] lg:h-[48px] rounded border-[1px] border-sky-gray-500 focus:outline-none focus:border-[#DA1E28] placeholder:text-base placeholder:text-sky-gray-120 placeholder:p-4'>
+                    <option className='text-base text-sky-gray-120 p-4'>Mes</option>
+                    <option className='text-base text-sky-gray-120 p-4' value="01">01</option>
+                    <option className='text-base text-sky-gray-120 p-4' value="02">02</option>
+                    <option className='text-base text-sky-gray-120 p-4' value="03">03</option>
+                    <option className='text-base text-sky-gray-120 p-4' value="04">04</option>
+                    <option className='text-base text-sky-gray-120 p-4' value="05">05</option>
+                    <option className='text-base text-sky-gray-120 p-4' value="06">06</option>
+                    <option className='text-base text-sky-gray-120 p-4' value="07">07</option>
+                    <option className='text-base text-sky-gray-120 p-4' value="08">08</option>
+                    <option className='text-base text-sky-gray-120 p-4' value="09">09</option>
+                    <option className='text-base text-sky-gray-120 p-4' value="10">10</option>
+                    <option className='text-base text-sky-gray-120 p-4' value="11">11</option>
+                    <option className='text-base text-sky-gray-120 p-4' value="12">12</option>
+                  </select>
+                </div>
+              </div>
+              <div className='flex flex-col h-[86px] lg:w-[205px] lg:h-[80px]'>
+                <div className='block my-1 lg:mt-0 lg:mb-1'>
+                  <label className='text-sm text-[#292929]'>Año de vencimiento</label>
+                </div>
+                <div className='block my-1 lg:mb-0 lg:mt-1'>
+                  <input type='tel' name='anioVenci' placeholder='Año' className='w-full h-[40px] lg:h-[48px] rounded border-[1px] border-sky-gray-500 focus:outline-none focus:border-[#DA1E28] placeholder:text-base placeholder:text-sky-gray-120 placeholder:p-4' />
+                </div>
+              </div>
+              <div className='flex flex-col h-[86px] lg:w-[205px] lg:h-[80px]'>
+                <div className='block my-1 lg:mt-0 lg:mb-1'>
+                  <label className='text-sm text-[#292929]'>CVV</label>
+                </div>
+                <div className='block my-1 lg:mb-0 lg:mt-1'>
+                  <input type='password' name='apellidoMaterno' placeholder='CVV' maxLength={3} className='w-full h-[40px] lg:h-[48px] rounded border-[1px] border-sky-gray-500 focus:outline-none focus:border-[#DA1E28] placeholder:text-base placeholder:text-sky-gray-120 placeholder:p-4' />
+                </div>
+              </div>
+            </div>
+            
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda a quidem enim, quibusdam laborum impedit in soluta similique, repellat dolore neque quas distinctio? Molestiae ab impedit officia odio placeat earum.
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda a quidem enim, quibusdam laborum impedit in soluta similique, repellat dolore neque quas distinctio? Molestiae ab impedit officia odio placeat earum.
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda a quidem enim, quibusdam laborum impedit in soluta similique, repellat dolore neque quas distinctio? Molestiae ab impedit officia odio placeat earum.
